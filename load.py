bdf42cb90be0b71410d563ebc5cb07ede31a114e
Ivan_Kashtan <ivankashtalapov@yandex.ru>
2025-02-23 15:44:07 +0700

    rep

---
+++ load.py
@@ -0,0 +1,35 @@
+from numpy._core._multiarray_umath import ceil
+
+from in_dat import *
+from gen import q_g, s_g
+# from in_dat import p_r
+
+# Мощности нагрузок потребителей
+# Максимальная в зимний период
+p_ld_max_w = n_ld * p_ld*k_s
+tg_ld = (1 - cos_ld**2)**(1/2)/cos_ld
+q_ld_max_w = p_ld_max_w*tg_ld
+s_ld_max_w = p_ld_max_w / cos_ld
+# Максимальная в летний период
+p_ld_max_s = p_ld_max_w * k_max_sm
+q_ld_max_s = q_ld_max_w * k_max_sm
+s_ld_max_s = s_ld_max_w * k_max_sm
+
+p_ld_min_sm = p_ld_max_w * k_min_sm
+q_ld_min_sm = q_ld_max_w * k_min_sm
+s_ld_min_sm = s_ld_max_w * k_min_sm
+# Мощности собственных нужд
+p_s_n = k_s_n * p_g
+q_s_n = k_s_n * q_g
+s_s_n = k_s_n * s_g
+
+p_g_ = (1-k_s_n) * p_g
+q_g_ = (1-k_s_n) * q_g
+s_g_ = (1-k_s_n) * s_g  # 875
+# ТНЦ-1000000/330, 347/24
+n1 = int(ceil(p_ld_max_w / p_g_))  # число генераторов, работающих на сторону СН
+# n1 = ceil(85/27)
+# n1 = ceil(85/27)
+
+# print(f'{s_g_:.1f}')
+# print(f'{n1:.1f}')
---
+++ var2.py
@@ -0,0 +1,32 @@
+# – к стороне СН подключено 4 генератора (n1 = 4), к автотрансформаторам связи генераторы не подключены.
+from math import sqrt
+
+from gen import cos_g
+from load import *
+
+n1_c = 4
+
+p1 = n1_c * p_g_ - p_ld_min_sm
+q1 = n1_c * q_g_ - q_ld_min_sm
+s1 = sqrt(p1**2 + q1**2) / 2
+
+p2 = p1 - p_r
+q2 = q1 - p_r * (1 - cos_g**2)**(1/2)/cos_g
+k_r = 1.2
+s2 = sqrt(p2**2 + q2**2) / k_r
+
+p3 = p_ld_max_w - (n1_c - 1)*p_g_ + p_s_n
+q3 = q_ld_max_w - (n1_c - 1)*q_g_ + q_s_n
+s3 = sqrt(p3**2 + q3**2) / 2
+
+p4 = p_ld_max_s - (n1_c - 2)*p_g_ + 2*p_s_n
+q4 = q_ld_max_s - (n1_c - 2)*q_g_ + 2*q_s_n
+s4 = sqrt(p3**2 + q3**2) / 2
+
+s_c = max(s1, s2, s3, s4)
+# 1048
+
+# print(int(ceil(s1)))
+# print(int(ceil(s2)))
+# print(int(ceil(s3)))
+print(int(ceil(s_c)))
